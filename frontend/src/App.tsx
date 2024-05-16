92f4cdf2b17779491322f8ce78a88d94ba3721b4
Victor <vav0701@mail.ru>
2024-05-16 21:01:50 +0500

    ui realization of get and post requests + sample env

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,60 @@
-function App() {
-    return (
-        <>
-        </>
-    )
-}
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default App
+type TDeck = {
+  title: string;
+  _id: string;
+};
+
+export default function App() {
+  const [decks, setDecks] = useState<TDeck[]>([]);
+  const [title, setTitle] = useState<string>("");
+
+  const handleCreateDeck = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:5000/decks", {
+        title: title,
+      })
+      .then((r) => {
+        console.log(r);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setTitle("");
+  };
+
+  useEffect(() => {
+    async function fetchDecks() {
+      await axios
+        .get("http://localhost:5000/decks")
+        .then((response) => setDecks(response.data));
+    }
+    fetchDecks().catch((err) => {
+      console.log(err);
+    });
+  }, [decks]);
+
+  return (
+    <>
+      <ul className="decks">
+        {decks.map((cur, index) => {
+          return <li key={index}>{cur.title}</li>;
+        })}
+      </ul>
+      <form onSubmit={handleCreateDeck}>
+        <label htmlFor="deck-title">Deck Title</label>
+        <input
+          id="deck-title"
+          type="text"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button>Create deck</button>
+      </form>
+    </>
+  );
+}
